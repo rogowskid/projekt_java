0f25a9194326a95f31f3da468770a13b665e429d
rogowskid <rogowskid11@gmail.com>
2021-04-03 20:41:59 +0200

    Zmiana w displayText

--- src/saperPackage/main.java
+++ src/saperPackage/main.java
@@ -3,5 +3,18 @@ package saperPackage;
 public class main {
     public static void main(String[] args) {
         System.out.println("hello");
+
+
+
+
+
     }
+
+
+    private void displayText(){
+        System.out.println("Siema");
+
+    }
+
+
 }
