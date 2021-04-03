4f5d36e673ee6ea3bd0bec34477e08a5e143c90b
rogowskid <rogowskid11@gmail.com>
2021-04-03 23:03:17 +0200

    Daniel nowa klasa

---
+++ src/saperPackage/ranking.java
@@ -0,0 +1,19 @@
+package saperPackage;
+
+import java.awt.event.ActionEvent;
+
+public class ranking extends startFrame{
+
+    public ranking() {
+        super();
+
+        frame.getContentPane().removeAll();
+        frame.repaint();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        super.actionPerformed(e);
+    }
+}
--- src/saperPackage/startFrame.java
+++ src/saperPackage/startFrame.java
@@ -5,22 +5,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class startFrame {
+public class startFrame implements ActionListener {
     public JFrame frame;
     public JPanel panel;
     public JPanel panel2;
     public JButton startG;
     public JButton ranking;
+    public JButton quitGame;
+    public ranking Ranking;
+
     public startFrame(){
 
         panel = new JPanel();
         frame = new JFrame();
-        panel2 = new JPanel();
+
         //definicja przyciskow
         startG = new JButton("Start Game"); //definicja przycisku Start Game
         ranking = new JButton("Ranking");
-        panel.add(startG);
-        panel2.add(ranking);
+        quitGame = new JButton("Quit");
+
+
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Wczytanie znaku zamkniecie w prawym rogu
         frame.setTitle("Saper"); //Nazwa okna
@@ -28,13 +32,49 @@ public class startFrame {
         frame.setSize(700,700); //ustawienie szerokosci jak i wysokosci okna
         frame.setVisible(true); //true oznacza ze okno jest widczone
         panel.setBorder(BorderFactory.createEmptyBorder(100, 0, 500, 0));
-        panel2.setBorder(BorderFactory.createEmptyBorder(200, 0, 500, 0));
+        panel.setLayout(null);
+        panel.add(startG);
+        panel.add(ranking);
+        panel.add(quitGame);
+        startG.setBounds(300,100,100,40);
+        ranking.setBounds(300,150,100,40);
+        quitGame.setBounds(300,200,100,40);
         frame.add(panel);
-        frame.add(panel2);
-
+        frame.setResizable(false);
+        startG.addActionListener(this);
+        ranking.addActionListener(this);
+        quitGame.addActionListener(this);
 
         System.out.println("KOONIEC");
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        if (source == startG) {
+            frame.getContentPane().removeAll();
+            frame.repaint();
+
+        }
+
+        if (source == ranking)
+        {
+
+            frame.getContentPane().removeAll();
+            frame.repaint();
+
+            Ranking = new ranking();
+
+
+        }
+
+        if (source == quitGame)
+        {
+
+            frame.dispose();
+        }
+
+    }
 }
